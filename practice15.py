6d9c20466c3d7fe2ccd0ee6d0f8cfa54a4a487d7
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2021-10-12 22:29:22 +0600

    Create practice15.py

---
+++ practice15.py
@@ -0,0 +1,22 @@
+str = "Hello World"
+str = "I am bot"
+print(str[0],str[5])
+
+
+str = "Hello World"
+print(str[-4])
+
+
+str = "K"
+print(ord(str))
+
+
+str = "cab"
+#print(ord(str))
+
+
+print(
+    "wow! great! stunning!".count("!"),
+    "wow! great! stunning!".count("!",10,20),
+    "wow! great! stunning!".count("!",10)
+)
