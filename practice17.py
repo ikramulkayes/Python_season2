1ebb1b02e0ab95efcc8af2ea3f69e8e6b1b340c6
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2021-10-21 16:26:10 +0600

    Create practice17.py

---
+++ practice17.py
@@ -0,0 +1,15 @@
+dict1 = {"first_name": "frodo","last_name":"baggins"}
+#x = dict1.pop("lst_name")
+#print(x)
+
+a = {"snack": "pasta","desert":"brownie","main":"rice"}
+b = a.copy()
+b["desert"]= "pudding"
+print(a)
+
+mytup = (10000,)
+print(mytup*6)
+
+subject = ("physics","chemistry","history","anthropology","archeology")
+subjects = subject + subject
+print(subjects)
