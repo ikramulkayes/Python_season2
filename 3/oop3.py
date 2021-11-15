78be98a85da6072bb31696ee22223d1ca6b3dcb0
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2021-11-15 23:03:46 +0600

    Create oop3.py

---
+++ 3/oop3.py
@@ -0,0 +1,30 @@
+class Wadiya():
+    def __init__(self):
+        self.name = 'Aladeen'
+        self.designation = 'President Prime Minister Admiral General'
+        self.num_of_wife = 100
+        self.dictator = True
+
+#Write your code for subtask 1, 2, 3 and 4 here
+#subtask 1
+wadiya = Wadiya()
+
+#subtask 2
+print("Part 1:")
+print("Name of Presidnet:",wadiya.name)
+print("Designation:",wadiya.designation)
+print("Number of wife:",wadiya.num_of_wife)
+print("Is he/she a dictator:",wadiya.dictator)
+
+#subtask 3
+print("Part 2:")
+wadiya.name = "Donald Trump"
+wadiya.designation = "President"
+wadiya.num_of_wife = 1
+wadiya.dictator = False
+print("Name of Presidnet:",wadiya.name)
+print("Designation:",wadiya.designation)
+print("Number of wife:",wadiya.num_of_wife)
+print("Is he/she a dictator:",wadiya.dictator)
+#subtask 4
+print("previous information lost")
