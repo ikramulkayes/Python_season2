9997b019c62ab37ee8eb74788caf20d316a9616e
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2021-11-15 23:03:43 +0600

    Create oop2.py

---
+++ 3/oop2.py
@@ -0,0 +1,34 @@
+class Flower():
+    def __init__(self):
+        self.name = ""
+        self.color = ""
+        self.num_of_peta = ""
+    
+
+
+
+
+
+
+#Write your class code here
+flower1 = Flower()
+flower1.name="Rose"
+flower1.color="Red"
+flower1.num_of_petal=6
+print("Name of this flower:", flower1.name)
+print("Color of this flower:",flower1.color)
+print("Number of petal:",flower1.num_of_petal)
+print("=====================")
+flower2 = Flower()
+flower2.name="Orchid"
+flower2.color="Purple"
+flower2.num_of_petal=4
+print("Name of this flower:",flower2.name)
+print("Color of this flower:",flower2.color)
+print ("Number of petal:",flower2. num_of_petal)
+#Write the code for subtask 2 and 3 here
+#subtask 2
+print(flower1)
+print(flower2)
+#subtask 3
+print("they are different")
