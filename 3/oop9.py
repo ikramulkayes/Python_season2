de312719ca595ffee2c0a4a21e3f2ca038319e81
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2021-11-15 23:04:01 +0600

    Create oop9.py

---
+++ 3/oop9.py
@@ -0,0 +1,43 @@
+class box:
+    def __init__(self,lst):
+        self.height = lst[0]
+        self.width = lst[1]
+        self.breadth = lst[2]
+        print("Creating a Box!")
+        sum1 = self.height * self.width * self.breadth
+        print(f"Volume of the box is {sum1} cubic units.")
+
+
+
+
+
+
+
+
+
+
+
+print("Box 1")
+b1 = box([10,10,10])
+print("=========================")
+print("Height:", b1.height)
+print("Width:", b1.width)
+print("Breadth:", b1.breadth)
+print("-------------------------")
+print("Box 2")
+b2 = box((30,10,10))
+print("=========================")
+print("Height:", b2.height)
+print("Width:", b2.width)
+print("Breadth:", b2.breadth)
+b2.height = 300
+print("Updating Box 2!")
+print("Height:", b2.height)
+print("Width:", b2.width)
+print("Breadth:", b2.breadth)
+print("-------------------------")
+print("Box 3")
+b3 = b2
+print("Height:", b3.height)
+print("Width:", b3.width)
+print("Breadth:", b3.breadth)
