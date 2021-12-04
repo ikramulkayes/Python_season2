e3c77b1111ee3a16069270b78149b4314aca11da
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2021-12-05 02:55:12 +0600

    Create practice62.py

---
+++ practice62.py
@@ -0,0 +1,7 @@
+class royal:
+    name = "elizabeth"
+    @classmethod
+    def intro(cls,msg):
+        print("Welcome your majesty",name)
+royal.name = "victoria"
+royal.intro("Welcome to British Royal family",name)
