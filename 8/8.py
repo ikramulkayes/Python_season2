fb8fa5781d134d84638aa0c0ece1b89a929ed8f7
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2022-01-10 11:52:42 +0600

    Create 8.py

---
+++ 8/8.py
@@ -0,0 +1,41 @@
+class Team:
+    def __init__(self, name):
+        self.name = "default"
+        self.total_player = 5
+    def info(self):
+        print("We love sports")
+# Write your code here.
+
+class FootBallTeam(Team):
+    def __init__(self, name):
+        super().__init__(name)
+        self.name = name
+        self.total_player=11
+    def info(self):
+        
+        print(f"Our name is {self.name}")
+        print("We play Football")
+        return super().info()
+class CricketTeam(Team):
+    def __init__(self, name):
+        super().__init__(name)
+        self.name = name
+        self.total_player=11
+    def info(self):
+        
+        print(f"Our name is {self.name}")
+        print("We play Cricket")
+        return super().info()
+
+
+
+class Team_test:
+    def check(self, tm):
+        print("=========================")
+        print("Total Player:", tm.total_player)
+        tm.info()
+f = FootBallTeam("Brazil")
+c = CricketTeam("Bangladesh")
+test = Team_test()
+test.check(f)
+test.check(c)
