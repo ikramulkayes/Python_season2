6e43619b1f824fd0b8f59ddcc8040a79234253ec
Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2022-02-03 01:51:28 +0600

    Create 3.py

---
+++ Finals Practice/3.py
@@ -0,0 +1,28 @@
+class book:
+    def __init__(self, name):
+        self.name = name
+        self.genre='biography'
+    def review(self):
+         print('This book is just out of the world,mind-blowing!')
+
+class fiction(book):
+    def __init__(self, name,genre):
+        super().__init__(name)
+        self.genre = genre
+    def review(self):
+        print(f"{self.name} which is a {self.genre} is just out of the world, mind-blowing!")
+
+class nonfiction(book):
+    def __init__(self, name):
+        super().__init__(name)
+    def review(self):
+        print(f"{self.name} which is a {self.genre} is just out of the world, mind-blowing!")
+
+# Write your code here        
+
+b1 = fiction('The Shining','Psychological horror')
+b2 = nonfiction('A Beautiful Mind')
+b1.review()
+print('=========================')
+b2.review()
+print('=========================')
